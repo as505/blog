e7911191d844d642e9275f29b0ceace3901ff972
as505 <aso199@uit.no>
2026-02-15 16:19:05 +0100

    setting color pallete

--- blog/src/routes/+layout.svelte
+++ blog/src/routes/+layout.svelte
@@ -14,3 +14,24 @@
 </nav>
 
 {@render children()}
+
+<style>
+	:global(html) {
+		--col-bg: rgb(38, 38, 38);
+		--col-highlight: rgb(226, 232, 206);
+		--col-content: rgb(172, 191, 164);
+		--col-header: rgb(255, 127, 17);
+		background-color: var(--col-bg);
+	}
+	:global(h1) {
+		color: var(--col-header);
+	}
+
+	:global(a) {
+		color: var(--col-highlight);
+	}
+
+	:global(body) {
+		color: var(--col-content);
+	}
+</style>
