3a2dc7fa3c1cd1e0c9cc24c9ff8e8a4a79db3a6e
as505 <aso199@uit.no>
2026-02-16 18:12:48 +0100

    fixed scaling on css elements

--- blog/src/routes/blog/+layout.svelte
+++ blog/src/routes/blog/+layout.svelte
@@ -7,13 +7,17 @@
     </div>
     <article class="text"></article>
     <div class="frit-Contain">
+        <div class="frit" id="frit3-r"></div>
+        <div class="frit" id="frit2-r"></div>
+        <div class="frit" id="frit1-r"></div>
+        <div class="frit" id="frit0-r"></div>
     </div>
 </div>
 
 
 <style>
 .layout{
-    grid-template-columns: 25vw 40vw 25vw;
+    grid-template-columns: 20vw 40vw 20vw;
     grid-template-rows: repeat(2, 100vh);
     width:90vw;
     height: 100vh;
@@ -27,50 +31,77 @@
 
 .frit-Contain{
     position: relative;
+    display: flex;
 }
 
 .frit{
     position: absolute;
     height: 100%;
     background-color: rgba(0, 0, 0, 0);
+    background-size: 4rem 4rem;
     background-image:  radial-gradient(circle, var(--col-content), var(--col-content)70%, rgba(0, 0, 0, 0)70%);
+    width: 4rem;
     background-repeat: repeat-y;
 }
 
 #frit1{
-    right: 0%;
-    width: 20px;
-    background-size: 40px 40px;
-    background-position: 0px 0px;
+    right: -0.3rem;
+    width: 1.4rem;
+    background-position: 0rem 0rem;
+}
+
+#frit1-r{
+    left: -0.3rem;
+    width: 1.4rem;
+    background-position: -2.6rem 0rem;
 }
 
 #frit2{
-    right: 3.5%;
-    width: 40px;
-    background-size: 40px 40px;
+    right: 0.6rem;
+    background-size: 4rem 4rem;
     background-image:  radial-gradient(circle, var(--col-content), var(--col-content)40%, rgba(0, 0, 0, 0)40%);
-    background-position: 4px 20px;
+    background-position: 0.4rem 10rem;
+}
+
+#frit2-r{
+    left: 0.6rem;
+    background-size: 4rem 4rem;
+    background-image:  radial-gradient(circle, var(--col-content), var(--col-content)40%, rgba(0, 0, 0, 0)40%);
+    background-position: -0.4rem 10rem;
 }
 
 #frit3{
-    right: 8%;
-    width: 40px;
-    background-size: 40px 40px;
+    right: 2.3rem;
+    background-size: 4rem 4rem;
     background-image:  radial-gradient(circle, var(--col-content), var(--col-content)20%, rgba(0, 0, 0, 0)20%);
-    background-position: 4px 0px;
+    background-position: 0.4rem 0rem;
+}
+
+#frit3-r{
+    left: 2.3rem;
+    background-size: 4rem 4rem;
+    background-image:  radial-gradient(circle, var(--col-content), var(--col-content)20%, rgba(0, 0, 0, 0)20%);
+    background-position: -0.4rem 0rem;
 }
 
 #frit0{
-    right: -5%;
-    width: 40px;
-    background-size: 40px 40px;
-    background-image:  radial-gradient(circle, var(--col-bg), var(--col-bg)20%, var(--col-content)20%);
-    background-position: 4px 0px;
+    right: -2rem;
+    width: 2rem;
+    background-size: 4rem 4rem;
+    background-image:  radial-gradient(circle, var(--col-bg), var(--col-bg)20%, rgba(0, 0, 0, 0)20%);
+    background-position: -1.0rem 0rem;
 }
 
 
+#frit0-r{
+    left: -2rem;
+    width: 2rem;
+    background-size: 4rem 4rem;
+    background-image:  radial-gradient(circle, var(--col-bg), var(--col-bg)20%, rgba(0, 0, 0, 0)20%);
+    background-position: -1.0rem 0rem;
+}
+
 .text{
-    grid-area: "article";
     background-color: var(--col-content);
 }
 
