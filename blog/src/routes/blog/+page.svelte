60e8869ce96ff3f660549cea651e667b45f1d441
as505 <aso199@uit.no>
2026-02-18 21:12:34 +0100

    first draft, minus test results and conclution

--- blog/src/routes/blog/+page.svelte
+++ blog/src/routes/blog/+page.svelte
@@ -20,11 +20,11 @@
                 LZW algorithms works by creating a dictionary of 'codes' that describe patterns found in the input.
                 The LZW implementation for Gif files use variable length codes to ensure as few bits of data are used as possible.
             </p>
-            <h2 class="example">
+            <h3 class="example">
                 Example:<br>
                 If the code dictionary only has 2 entries, a single bit is enough to refer to all indexes. 
                 When a third entry is added to the dictionary, each subsequent code needs atleast 2 bits to refer to any entry.
-            </h2>
+            </h3>
             <p>
                 As more entries are added to the dictionary, more bits are needed to index it. 
                 Therefore, to implement Gif LZW, we need a way to get the exact number of bits needed to represent any number.
@@ -34,13 +34,49 @@
                 N^2 represents the largest possible number that N bits can describe, so
                 our problem can be described as:
             </p>
-            <h2>
+            <h3>
                 For any number M, what is the smallest number N, where N^2 &gt= M;
-            </h2>
+            </h3>
             <p>
-                For fun, and a little curiosity, I wrote and compared 3 functions to do this, 
+                For fun, and a little curiosity, I wrote and compared 3 functions, 
                 comparing them to see if there were any noticable differences in performance.
             </p>
+            <hr>
+            <h2>
+                The experiment
+            </h2>
+            <p>
+                The first function uses a 32 bit integer, where all the bits are set to 1.
+                In a loop, the integer is bitshifted to the right until only one digit remain.
+                The amount of digits remaining is increased by one digit each iteration of the loop,
+                until we have a value greater than or equal to the input.
+            </p>
+            <p>
+                The seccond function attempts to use a binary search to reduce the number of iterations in the loop.
+                Instead of including one more digit each iteration, it instead tries to include 16 digits.
+                I call these bits the pivot.
+                If 16 digits is too big of a jump, it halves the pivot to 8 bits, then 4, 2, and finaly 1 bit.
+                Instead of returning when the bitshifted value &gt= input, it returns when the pivot is reduced to 0 bits.
+            </p>
+            <p>
+                The final function is a simple loop where an int is raised to the power of 2.
+                As long as this equation gives a number smaller than the input, we increment the int by 1.
+            </p>
+            <p>
+                Since we can pass pointers to functions in C, we can write a function to time the execution of any given function.
+                To get a measurable number in MS, we run each function several thousands of times.
+                Each of the thousands of runs we generate a random Int as the input.
+                The functions generating the random integers can also be customized to favor smaller or larger numbers,
+                so we can test how the performance is affected by the size of the input int.
+            </p>
+            <hr>
+            <h2>
+                Results
+            </h2>
+            <hr>
+            <h2>
+                Conclusion
+            </h2>
         </article>
     </div>
     <MarginR></MarginR>
@@ -62,6 +98,16 @@ h1{
     text-align: center;
     margin-left: auto;
     margin-right: auto;
+}
+
+h2{
+    color: var(--col-header);
+    font-size: 2.5rem;
+}
+
+h3{
+    color: var(--col-header);
+    font-size: 1.5rem;
 }
 
 p{
